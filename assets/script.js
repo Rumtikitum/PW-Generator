b43793fc7012ebb12e25b669a4f9ef21c6ce95d7
Rumtikitum <rt.student.only@gmail.com>
2021-05-09 07:04:36 -0700

    finalized code. All night ordeal

--- assets/script.js
+++ assets/script.js
@@ -1,64 +1,76 @@
-//Primary initiating function to log user response. Confirms choices.
+//pulling elements from HTML (button responses) to support website functionality
+const passwordEl = document.getElementById('password');
+const lengthEl = document.getElementById('length');
+const ucEl = document.getElementById('uC');
+const lcEl = document.getElementById('lC');
+const numEl = document.getElementById('num');
+const specEl = document.getElementById('specChar');
+const generateEl = document.getElementById('generate');
+//
 
-var promptingPW = function() {
-  // How long?
-  window.alert("Welcome to BEST PASSWORD GENERATOR 7k! Allow me to assist you today!")
-
-  console.log(promptLength)
-  var promptLength = window.prompt("How long would you like your password? Between 8-128 characters")
-    window.alert("Great. We will have a " + promptLength + " character long password")
-
-
-  // Upper case confirmation
-  var confirmUpper = window.confirm ("Would you like your password to have uppercase letters?")
-    if (confirmUpper) {
-      window.alert("Perfect we will include uppercase letters")
-    } else {
-      window.alert("No uppercases will be used")
-    } 
-
-  // Lower case confirmation
-  var confirmLower = window.confirm ("Would you like your password to contain lower case letters?")
-    if (confirmLower) {
-      window.alert("Perfect we will include lowercase letters")
-    } else {
-      window.alert("No lowercases will be used")
-    } 
-
-  //Number Confirmation
-  var confirmNum = window.confirm ("Would you like to have numbers in your password?")
-    if (confirmNum) {
-      window.alert("Perfect we will include Numbers")
-    } else {
-      window.alert("No Numbers will be used")
-    } 
-
-  var confirmSpec = window.confirm ("Would you like to have special characters in your password? ex. @#$%^")
-    if (confirmSpec) {
-      window.alert("Perfect we will include special characters")
-    } else {
-      window.alert("No special characters will be used")
-    } 
-
-  window.alert("Please now click the button 'Generate Password'")
+//establishing randomized functions to help pull characters. Referred to browser character code
+function lowercaseRandomizer() {
+    const lowerAlpha = 'qwertyuiopasdfghjklzxcvbnm'
+	return lowerAlpha[Math.floor(Math.random() * lowerAlpha.length)]
+}
+function uppercaseRandomizer() {
+    const upperAlpha = 'QWERTYUIOPASDFGHJKLZXCVBNM'
+	return upperAlpha[Math.floor(Math.random()* upperAlpha.length)]
+}
+function numberRandomizer() {
+    const numberRand = '123456789'
+	return numberRand[Math.floor(Math.random() * numberRand.length)];
+}
+function specialCharacter() {
+	const symbols = '!@#$%^&*(){}[]=<>/,.'
+	return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-promptingPW();
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+//bunching the randomized functions to simplify coding
+const randomizer = {
+	lowercaseRandomizer,
+	uppercaseRandomizer,
+	numberRandomizer,
+	specialCharacter
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//establishing functionality when we click out utilizing the id=generate
+generate.addEventListener('click', () => {
+	const confirmLow = lcEl.checked;
+    const confirmSpecChar = specEl.checked;
+    const length = +lengthEl.value;
+	const confirmNum = numEl.checked;
+	const confirmUp = ucEl.checked;
+    
+    //when button is clicked, password will populate in the textbox
+	passwordEl.innerText = generatePassword(confirmLow, confirmUp, confirmNum, confirmSpecChar, length);    
+});
+
+//Getting all the code set up for the "Generate Password" button functionality
+//Establishing functions for site
+function generatePassword(lowercaseRandomizer, uppercaseRandomizer, numberRandomizer, specialCharacter, length) {
+	let generatedPassword = '';
+    //identify which buttons were confirmed
+	const customConfirm = lowercaseRandomizer + uppercaseRandomizer + numberRandomizer + specialCharacter;
+    //counting for each customization. Establishing which is true or false. Preparing for Loop
+	const customArray = [{lowercaseRandomizer}, {uppercaseRandomizer}, {numberRandomizer}, {specialCharacter}].filter(item => Object.values(item)[0]);
+	console.log('customArray: ', customArray)
+	// Error notice in the case 
+	if(customConfirm === 0) {
+		return 'ERROR! Must at least select one option to Generate Password.';
+	}
+	
+	//creating loop for password generator
+	for(let i=0; i<length; i+=customConfirm) {
+		customArray.forEach(type => {
+			const genPass = Object.keys(type)[0];
+			generatedPassword += randomizer[genPass]();
+		});
+	}
+	
+    //modified answer. For some reason when clicking uppercase, lowercase & numbers you would get an extra number. Only solution I currently found thanks to google....
+	const correctedArray = generatedPassword.slice(0, length);
+	
+	return correctedArray;
+}
